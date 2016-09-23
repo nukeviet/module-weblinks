bfadd12d1a2c1db4f1bda55d7879468ebec6b63c
Phan Tan Dung <phantandung92@gmail.com>
2016-09-23 08:34:06 +0700

    Gói cập nhật

---
+++ README.md
@@ -0,0 +1,17 @@
+# Hướng dẫn cập nhật module weblinks từ 4.0.29 lên 4.1.00
+
+Chú ý: 
+- Gói cập nhật này chỉ dành cho module weblinks 4.0.29, nếu module của bạn không ở phiên bản này cần tìm các hướng dẫn cập nhật lên 4.0.29 trước.
+- Module weblinks 4.1.00 hoạt động tốt trên NukeViet 4 RC3, NukeViet 4 Official và NukeViet 4.1 Beta
+
+## Chuẩn bị cập nhật
+
+Backup toàn bộ CSDL dữ liệu và code của site đề phòng rủi ro.
+
+## Thực hiện cập nhật
+
+Đăng nhập quản trị site, di chuyển vào khu vực Công cụ web => Kiểm tra phiên bản, tại đây nếu hệ thống kiểm tra được module weblinks và có yêu cầu cập nhật hãy tiến hành theo hướng dẫn của hệ thống.
+
+Nếu không cập nhật được theo cách trên hãy thực hiện cập nhật thủ công như sau:
+
+Tải gói cập nhật tại https://github.com/nukeviet/module-weblinks/releases/download/4.1.00/update-to-4.1.00.zip. Giải nén và upload thư mục install lên ngang hàng với thư mục install trên server. Đăng nhập quản trị site, nhận được thông báo cập nhật và tiến hành cập nhật theo hướng dẫn của hệ thống.
---
+++ install/update_data.php
@@ -0,0 +1,141 @@
+<?php
+
+/**
+ * @Project NUKEVIET 4.x
+ * @Author VINADES.,JSC (contact@vinades.vn)
+ * @Copyright (C) 2012 VINADES.,JSC. All rights reserved
+ * @License GNU/GPL version 2 or any later version
+ * @Createdate 2-10-2010 20:59
+ */
+
+if (!defined('NV_IS_UPDATE'))
+    die('Stop!!!');
+
+$nv_update_config = array();
+
+// Kieu nang cap 1: Update; 2: Upgrade
+$nv_update_config['type'] = 1;
+
+// ID goi cap nhat
+$nv_update_config['packageID'] = 'NVUWEBLINKS4100';
+
+// Cap nhat cho module nao, de trong neu la cap nhat NukeViet, ten thu muc module neu la cap nhat module
+$nv_update_config['formodule'] = 'weblinks';
+
+// Thong tin phien ban, tac gia, ho tro
+$nv_update_config['release_date'] = 1474563600;
+$nv_update_config['author'] = 'VINADES.,JSC (contact@vinades.vn)';
+$nv_update_config['support_website'] = 'https://github.com/nukeviet/module-weblinks/tree/to-4.1.00';
+$nv_update_config['to_version'] = '4.1.00';
+$nv_update_config['allow_old_version'] = array('4.0.29');
+
+// 0:Nang cap bang tay, 1:Nang cap tu dong, 2:Nang cap nua tu dong
+$nv_update_config['update_auto_type'] = 1;
+
+$nv_update_config['lang'] = array();
+$nv_update_config['lang']['vi'] = array();
+
+// Tiếng Việt
+$nv_update_config['lang']['vi']['nv_up_finish'] = 'Đánh dấu phiên bản mới';
+
+$nv_update_config['tasklist'] = array();
+$nv_update_config['tasklist'][] = array(
+    'r' => '4.1.00',
+    'rq' => 1,
+    'l' => 'nv_up_finish',
+    'f' => 'nv_up_finish'
+);
+
+// Danh sach cac function
+/*
+Chuan hoa tra ve:
+array(
+'status' =>
+'complete' => 
+'next' =>
+'link' =>
+'lang' =>
+'message' =>
+);
+status: Trang thai tien trinh dang chay
+- 0: That bai
+- 1: Thanh cong
+complete: Trang thai hoan thanh tat ca tien trinh
+- 0: Chua hoan thanh tien trinh nay
+- 1: Da hoan thanh tien trinh nay
+next:
+- 0: Tiep tuc ham nay voi "link"
+- 1: Chuyen sang ham tiep theo
+link:
+- NO
+- Url to next loading
+lang:
+- ALL: Tat ca ngon ngu
+- NO: Khong co ngon ngu loi
+- LangKey: Ngon ngu bi loi vi,en,fr ...
+message:
+- Any message
+Duoc ho tro boi bien $nv_update_baseurl de load lai nhieu lan mot function
+Kieu cap nhat module duoc ho tro boi bien $old_module_version
+*/
+
+$array_modlang_update = array();
+$array_modtable_update = array();
+
+// Lay danh sach ngon ngu
+$result = $db->query("SELECT lang FROM " . $db_config['prefix'] . "_setup_language WHERE setup=1");
+while (list($_tmp) = $result->fetch(PDO::FETCH_NUM)) {
+    $array_modlang_update[$_tmp] = array("lang" => $_tmp, "mod" => array());
+
+    // Get all module
+    $result1 = $db->query("SELECT title, module_data FROM " . $db_config['prefix'] . "_" . $_tmp . "_modules WHERE module_file=" . $db->quote($nv_update_config['formodule']));
+    while (list($_modt, $_modd) = $result1->fetch(PDO::FETCH_NUM)) {
+        $array_modlang_update[$_tmp]['mod'][] = array("module_title" => $_modt, "module_data" => $_modd);
+        $array_modtable_update[] = $db_config['prefix'] . "_" . $_tmp . "_" . $_modd;
+    }
+}
+
+/**
+ * nv_up_finish()
+ *
+ * @return
+ *
+ */
+function nv_up_finish()
+{
+    global $nv_update_baseurl, $db, $db_config, $nv_Cache, $nv_update_config;
+
+    $return = array(
+        'status' => 1,
+        'complete' => 1,
+        'next' => 1,
+        'link' => 'NO',
+        'lang' => 'NO',
+        'message' => ''
+    );
+
+    try {
+        $num = $db->query("SELECT COUNT(*) FROM " . $db_config['prefix'] . "_setup_extensions WHERE basename='" . $nv_update_config['formodule'] . "' AND type='module'")->fetchColumn();
+        $version = "4.1.00 " . $nv_update_config['release_date'];
+        
+        if (!$num) {
+            $db->query("INSERT INTO " . $db_config['prefix'] . "_setup_extensions (
+                id, type, title, is_sys, is_virtual, basename, table_prefix, version, addtime, author, note
+            ) VALUES (
+                26, 'module', 'weblinks', 0, 1, 'weblinks', 'weblinks', '4.1.00 " . $nv_update_config['release_date'] . "', " . NV_CURRENTTIME . ", 'VINADES.,JSC (contact@vinades.vn)', 
+                'Hỗ trợ hỏi đáp'
+            )");
+        } else {
+            $db->query("UPDATE " . $db_config['prefix'] . "_setup_extensions SET 
+                id=26, 
+                version='" . $version . "', 
+                author='VINADES.,JSC (contact@vinades.vn)' 
+            WHERE basename='" . $nv_update_config['formodule'] . "' AND type='module'");
+        }
+    } catch (PDOException $e) {
+        trigger_error($e->getMessage());
+    }
+
+    $nv_Cache->delAll();
+    return $return;
+}
